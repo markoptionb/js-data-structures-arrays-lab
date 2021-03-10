27ac013fafb1ca92de04d753415a302b9bf9b8ca
markoptionb <optionbsupport@gmail.com>
2021-03-10 16:31:16 +1100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(){
+    return drivers.push("Ralph")
+};
+
+function destructivelyPrependDriver(){
+    return drivers.unshift("Bob")
+};
+
+function destructivelyRemoveLastDriver(){
+    return drivers.pop()
+};
+
+function destructivelyRemoveFirstDriver(){
+    return drivers.shift()
+};
+
+function appendDriver(){
+    return [...drivers, "Broom"]
+};
+
+function prependDriver(){
+    return ["Arnold", ...drivers]
+};
+
+function removeLastDriver(){
+    return [...drivers.slice(0, 2)]
+};
+
+function removeFirstDriver(){
+    return [...drivers.slice(1,3)]
+};
